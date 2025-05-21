ba8cbf52312c419139142984b0d62d8597390c21
audreymyiesha <audreymyiesha@gmail.com>
2025-05-21 09:38:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Selamat Datang di Web Informatika !")
 st.write(
-    "ngodingseru bersama Audrey"
+    "ngodingseru bersama Audreyy"
 )
 st.image("IMG_0393.jpeg", width=200)
